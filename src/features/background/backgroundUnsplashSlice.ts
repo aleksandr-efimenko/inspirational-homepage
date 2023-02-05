2d3c041c92a68d2f46779cf7faf93a739589362c
androidblog <androidblog@yandex.ru>
2023-02-05 16:09:23 +0400

    create unsplash backgound slice

---
+++ src/features/background/backgroundUnsplashSlice.ts
@@ -0,0 +1,18 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+
+}
+
+export const backgroundUnsplashSlice = createSlice({
+    name: 'backgroundUnsplash',
+    initialState: initialState,
+    reducers : {
+
+    },
+    extraReducers: (builder) => {
+        // builder
+        //     .addCase()
+    }
+})
+
