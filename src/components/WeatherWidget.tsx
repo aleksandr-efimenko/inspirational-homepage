c13632c3683266acf2f928d334c9556d609af70e
androidblog <androidblog@yandex.ru>
2023-02-11 17:23:30 +0400

    update img tag

--- src/components/WeatherWidget.tsx
+++ src/components/WeatherWidget.tsx
@@ -4,7 +4,7 @@ export default function WeatherWidget({ temperature, description, icon, unit, lo
     return (
         <>
             <div className='temp'>
-                {icon ? <img src={`http://openweathermap.org/img/w/${icon}.png`} alt='weather-pic'></img> : <></> }
+                {icon ? <img src={`http://openweathermap.org/img/w/${icon}.png`} alt='weather-pic' /> : <></> }
                 <p>{temperature} {unit}</p>
             </div>
             <p className='temp-description'>{location}</p>
