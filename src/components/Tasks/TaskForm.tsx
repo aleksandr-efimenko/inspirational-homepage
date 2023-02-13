e9d2343fe2b97dc16d21a4da3b9675e18d689d82
androidblog <androidblog@yandex.ru>
2023-02-13 11:10:05 +0400

    implemented useRef instead of setState for task text

--- src/components/Tasks/TaskForm.tsx
+++ src/components/Tasks/TaskForm.tsx
@@ -1,18 +1,18 @@
-import React, { FormEvent, useState } from 'react'
+import React, { FormEvent, useRef } from 'react'
 import './Tasks.css';
 import { useAppDispatch } from '../../app/hooks';
 import { addTask } from '../../features/tasks/tasksSlice';
 
 export default function TaskForm() {
     const dispatch = useAppDispatch();
-    const [newTaskText, setNewTaskText] = useState('');
-
+    const newTaskText = useRef<HTMLInputElement>(null);
+    const charLimit = 1000;
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
-        if (!newTaskText)
+        if (!newTaskText.current || !newTaskText.current.value)
             return;
-        dispatch(addTask(newTaskText))
-        setNewTaskText('');
+        dispatch(addTask(newTaskText.current.value.slice(0, charLimit)))
+        newTaskText.current.value = '';
     }
     return (
         <div className='task-form' >
@@ -20,10 +20,10 @@ export default function TaskForm() {
             <form onSubmit={handleSubmit}>
                 <div className='inputs'>
                     <input
+                        type='text'
                         id='task-text-input'
-                        onChange={e => setNewTaskText(e.target.value)}
-                        value={newTaskText}
-                        type='text'></input>
+                        ref={newTaskText}
+                        ></input>
                     <input type='submit' className='white-button'></input>
                 </div>
             </form>
