7890a6447c84cc906253a2c7d88830ad1781a96e
androidblog <androidblog@yandex.ru>
2023-02-19 10:34:36 +0400

    Add catch statement to fetch weather func

--- src/features/weather/weatherAPI.ts
+++ src/features/weather/weatherAPI.ts
@@ -1,6 +1,9 @@
 export const secretKey = process.env.REACT_APP_OPENWEATHER_KEY as string;
 
 export const fetchWeather =async (lon:number, lat: number) => {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${secretKey}`);
-    return response.json();
+    const response = 
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${secretKey}`)
+        .then(response => response.json())
+        .catch(error => console.log(error));
+    return response;
 }
