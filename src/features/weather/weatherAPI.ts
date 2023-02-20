e607389cce8eb741676dece7b7216c9025004062
androidblog <androidblog@yandex.ru>
2023-02-20 11:18:05 +0400

    Add function to get weather using City and Country Code

--- src/features/weather/weatherAPI.ts
+++ src/features/weather/weatherAPI.ts
@@ -1,9 +1,18 @@
 export const secretKey = process.env.REACT_APP_OPENWEATHER_KEY as string;
 
-export const fetchWeather =async (lon:number, lat: number) => {
-    const response = 
+export const fetchWeatherByLocation = async (lon: number, lat: number) => {
+    const response =
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${secretKey}`)
-        .then(response => response.json())
+            .then(response => response.json())
+            .catch(error => console.log(error));
+    return response;
+}
+
+export const fetchWeatherByCity = async (city: string, countryCode: string) => {
+    const response = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}}&units=metric&appid=${secretKey}`)
+        .then((response) => response.json())
         .catch(error => console.log(error));
     return response;
 }
+
+
