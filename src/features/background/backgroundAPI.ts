c8ecfd88a0336bc8e090ae282978751407b630b3
androidblog <androidblog@yandex.ru>
2023-02-21 12:03:42 +0400

    return if access key for background is not set

--- src/features/background/backgroundAPI.ts
+++ src/features/background/backgroundAPI.ts
@@ -4,11 +4,15 @@ const root = 'https://api.unsplash.com';
 const endPoint = '/photos/random';
 
 export const fetchBackgroundUnsplash = async (count: number = 15) => {
+    if (!accessKey) {
+        console.error('Access key not set');
+        // throw new Error('Access key not set');
+        return;
+    }
     try {
         const response = await fetch(`${root}${endPoint}?count=${count}&client_id=${accessKey}`);
         if (response.ok)
            return response.json();
-        console.log(response.ok)
     }
     catch (error) {
         console.warn(error);
