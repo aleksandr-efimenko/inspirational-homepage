08f211037b3939af82d46c768afb34055e2f8c05
androidblog <androidblog@yandex.ru>
2023-02-21 12:12:09 +0400

    return from weather api if secret key is not set

--- src/features/weather/weatherAPI.ts
+++ src/features/weather/weatherAPI.ts
@@ -1,15 +1,23 @@
-export const secretKey = process.env.REACT_APP_OPENWEATHER_KEY as string;
+export const accessKey = process.env.REACT_APP_OPENWEATHER_KEY as string;
 
 export const fetchWeatherByAutoLocation = async (lon: number, lat: number) => {
+    if (!accessKey) {
+        console.error('Access key OPENWEATHER is not set');
+        return;
+    }
     const response =
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${secretKey}`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${accessKey}`)
             .then(response => response.json())
             .catch(error => console.log(error));
     return response;
 }
 
 export const fetchWeatherByCity = async (city: string, countryCode: string) => {
-    const response = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}}&units=metric&appid=${secretKey}`)
+    if (!accessKey) {
+        console.error('Access key OPENWEATHER is not set');
+        return;
+    }
+    const response = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}}&units=metric&appid=${accessKey}`)
         .then((response) => response.json())
         .catch(error => console.log(error));
     return response;
