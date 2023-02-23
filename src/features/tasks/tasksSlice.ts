52dbf5fc105fde0ffe90f48599ff1d3d4416dcd1
androidblog <androidblog@yandex.ru>
2023-02-23 13:34:33 +0400

    create loading from firestore

--- src/features/tasks/tasksSlice.ts
+++ src/features/tasks/tasksSlice.ts
@@ -1,12 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 import { nanoid } from "nanoid";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../../app/firebase";
 
 export interface Task {
     text: string,
     id: string,
     done: boolean,
-    bgColor?: string
+    bgColor?: string,
+    uid?: string
 }
 
 export interface TasksState {
@@ -26,53 +29,74 @@ const getTasksFromLocalStorage = (key: string) => {
 }
 
 //Set local storage with 
-const setTasksInStorage = (tasks: Task[], key: string) => {
+const setTasksInBrowserStorage = (tasks: Task[], key: string) => {
     if (tasks) {
         localStorage.setItem(key, JSON.stringify(tasks))
     }
 }
 
-const initialState: TasksState = {
-    tasksList:
-        getTasksFromLocalStorage(key) ||
-        [{
-            text: 'Create new feature',
-            id: nanoid(),
-            done: false,
-            bgColor: generateBGColor()
-        },
-        {
-            text: 'Workout for 30 minutes',
-            id: nanoid(),
-            done: false,
-            bgColor: generateBGColor()
-        }],
+const setTaskInFirestore = async (task: Task) => {
+
 }
+
+const initialTaskList =  [{
+    text: 'Create new feature',
+    id: nanoid(),
+    done: false,
+    bgColor: generateBGColor()
+},
+{
+    text: 'Workout for 30 minutes',
+    id: nanoid(),
+    done: false,
+    bgColor: generateBGColor()
+}];
+
+const initialState: TasksState = {
+    tasksList: getTasksFromLocalStorage(key) || initialTaskList
+}
+
 export const tasksSlice = createSlice({
     name: 'tasks',
     initialState: initialState,
     reducers: {
         addTask: (state, action) => {
-            state.tasksList.push({
-                text: action.payload,
+            const newTask = {
+                text: action.payload.text,
                 id: nanoid(),
                 done: false,
-                bgColor: generateBGColor()
-            })
-            setTasksInStorage(state.tasksList, key);
+                bgColor: generateBGColor(),
+                uid: action.payload.uid
+            };
+            state.tasksList.push(newTask)
+
+            if (action.payload.uid) {
+                setDoc(doc(db, 'tasks', newTask.id), {
+                    ...newTask
+                }).catch(error => console.log(error));
+                setTasksInBrowserStorage(state.tasksList, key);
+            }
         },
         removeTask: (state, action) => {
             state.tasksList = state.tasksList.filter(el => el.id !== action.payload);
-            setTasksInStorage(state.tasksList, key);
+            setTasksInBrowserStorage(state.tasksList, key);
         },
         setTaskDone: (state, action) => {
             state.tasksList = state.tasksList.map(el => el.id === action.payload ? { ...el, done: true } : el);
-            setTasksInStorage(state.tasksList, key);
+            setTasksInBrowserStorage(state.tasksList, key);
+        },
+        setTasksFromFirestore: (state, action) => {
+            state.tasksList = action.payload;
+            setTasksInBrowserStorage(state.tasksList, key);
+        },
+        setTasksFromInitialState: (state) => {
+            state.tasksList = initialTaskList;
+            setTasksInBrowserStorage(state.tasksList, key);
         }
     }
 })
 
-export const { addTask, removeTask, setTaskDone } = tasksSlice.actions;
+export const { addTask, removeTask, setTaskDone, setTasksFromFirestore } = tasksSlice.actions;
 
 export const selectTaskList = (state: RootState) => state.tasks.tasksList;
 
