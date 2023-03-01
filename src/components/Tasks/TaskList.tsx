fc9a988bea33d958aaac1c8cbd73fb77027e9d3d
androidblog <androidblog@yandex.ru>
2023-03-01 20:11:48 +0400

    Separate local task list render from firestore

--- src/components/Tasks/TaskList.tsx
+++ src/components/Tasks/TaskList.tsx
@@ -4,7 +4,7 @@ import TaskComponent from './Task';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect } from 'react';
 import { auth, db } from '../../app/firebase';
-import { query, collection, where } from 'firebase/firestore';
+import { query, collection, where, DocumentData } from 'firebase/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore'
 import { Task, initializeTasksFromLocalStorage, selectTasksState } from '../../features/tasks/tasksSlice';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -17,7 +17,6 @@ export default function TaskList() {
   const [user] = useAuthState(auth);
   const receiptsQuery = query(collection(db, TASKS_COLLECTION), where("uid", "==", user?.uid || '-'));
   const [taskDocList, loading, error] = useCollectionData(receiptsQuery);
-  const taskList = user ? taskDocList : localTaskList;
 
   useEffect(() => {
     if (!user) {
@@ -25,32 +24,40 @@ export default function TaskList() {
     }
   }, [user, dispatch])
 
-  const renderTaskList = () => {
-    if (!loading && taskDocList) {
-      return <ul>
-        {
-          taskList?.map((el) => {
-            return <TaskComponent
-              bgColor={el.bgColor}
-              done={el.done}
-              text={el.text}
-              id={el.id}
-              key={el.id} />
-          })
-        }
-      </ul>
-    } else if (loading) {
+  const renderTasks = () => {
+    if (!user) {
+      return renderTaskList(localTaskList);
+    }
+
+    if (loading) {
       return <p><FontAwesomeIcon className='spinner' size={'2x'} icon={['fas', 'circle-notch']} /></p>
     } else if (error) {
       return <p>Error loading</p>
+    } else if (taskDocList) {
+      return renderTaskList(taskDocList);
     }
     else {
       return <p>No tasks</p>
     }
   }
+
+  const renderTaskList = (taskList: Task[] | DocumentData[]) => {
+    return <ul>
+      {
+        taskList?.map((el) => {
+          return <TaskComponent
+            bgColor={el.bgColor}
+            done={el.done}
+            text={el.text}
+            id={el.id}
+            key={el.id} />
+        })
+      }
+    </ul>
+  }
   return (
     <div className='task-list'>
-      {renderTaskList()}
+      {renderTasks()}
     </div >
   )
 }
