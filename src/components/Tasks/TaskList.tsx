5d72d83048cc061dce0bcfc8c0660e2b58d6a090
androidblog <androidblog@yandex.ru>
2023-03-02 15:24:35 +0400

    refractor tasklist, display tasks using ternary operators

--- src/components/Tasks/TaskList.tsx
+++ src/components/Tasks/TaskList.tsx
@@ -1,63 +1,67 @@
 import './Tasks.css';
+import { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { query, collection, where, DocumentData } from 'firebase/firestore';
+import { auth, db } from '../../app/firebase';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import TaskComponent from './Task';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useEffect } from 'react';
-import { auth, db } from '../../app/firebase';
-import { query, collection, where, DocumentData } from 'firebase/firestore';
-import { useCollectionData } from 'react-firebase-hooks/firestore'
-import { Task, initializeTasksFromLocalStorage, selectTasksState } from '../../features/tasks/tasksSlice';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { selectTasksState, initializeTasksFromLocalStorage, Task } from '../../features/tasks/tasksSlice';
+
+const TASKS_COLLECTION = "tasks";
+const FONT_ICON_SIZE = '2x';
 
 export default function TaskList() {
-  const localTaskList: Task[] = useAppSelector(selectTasksState).tasksList;
   const dispatch = useAppDispatch();
-
-  const TASKS_COLLECTION = "tasks";
+  const { tasksList } = useAppSelector(selectTasksState);
   const [user, userLoading] = useAuthState(auth);
-  const receiptsQuery = query(collection(db, TASKS_COLLECTION), where("uid", "==", user?.uid || '-'));
-  const [taskDocList, loading, error] = useCollectionData(receiptsQuery);
+
+  const tasksQuery = query(collection(db, TASKS_COLLECTION), where("uid", "==", user?.uid || '-'));
+  const [tasksData, loading, error] = useCollectionData(tasksQuery);
 
   useEffect(() => {
     if (!user) {
       dispatch(initializeTasksFromLocalStorage());
     }
-  }, [user, dispatch])
+  }, [user, dispatch]);
 
-  const renderTasks = () => {
+  const createTaskList = (taskList: Task[] | DocumentData[]) => {
+    return (
+      <ul>
+        {taskList?.map((task) => (
+          <TaskComponent
+            key={task.id}
+            id={task.id}
+            text={task.text}
+            done={task.done}
+            bgColor={task.bgColor}
+          />
+        ))}
+      </ul>
+    );
+  };
+
+  const displayTasks = () => {
     if (!user && !userLoading) {
-      return renderTaskList(localTaskList);
+      return createTaskList(tasksList);
     }
+    return (
+      <>
+        {loading ? (
+          <p>
+            <FontAwesomeIcon className='spinner' size={FONT_ICON_SIZE} icon={['fas', 'circle-notch']} />
+          </p>
+        ) : error ? (
+          <p>Error loading</p>
+        ) : tasksData ? (
+          createTaskList(tasksData)
+        ) : (
+          <p>No tasks</p>
+        )}
+      </>
+    );
+  };
 
-    if (loading) {
-      return <p><FontAwesomeIcon className='spinner' size={'2x'} icon={['fas', 'circle-notch']} /></p>
-    } else if (error) {
-      return <p>Error loading</p>
-    } else if (taskDocList) {
-      return renderTaskList(taskDocList);
-    }
-
-    return <p>No tasks</p>
-  }
-
-  const renderTaskList = (taskList: Task[] | DocumentData[]) => {
-    return <ul>
-      {
-        taskList?.map((el) => {
-          return <TaskComponent
-            bgColor={el.bgColor}
-            done={el.done}
-            text={el.text}
-            id={el.id}
-            key={el.id} />
-        })
-      }
-    </ul>
-  }
-  return (
-    <div className='task-list'>
-      {renderTasks()}
-    </div >
-  )
+  return <div className='task-list'>{displayTasks()}</div>;
 }
-
